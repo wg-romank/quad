356fa51873ffd1c40c9118bb321287084a8bcf33
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2023-05-31 23:05:21 +0900

    Swap usart pins for bluetooth

--- stm32-device/src/main.rs
+++ stm32-device/src/main.rs
@@ -11,7 +11,7 @@ mod app {
     use nalgebra::Vector3;
     use rtt_target::{rprintln, rtt_init_print};
 
-    use stm32f1xx_hal::device::USART1;
+    use stm32f1xx_hal::device::USART2;
     use stm32f1xx_hal::dma::CircBuffer;
     use stm32f1xx_hal::timer::{Timer, Tim4NoRemap, CountDownTimer, Event as TEvent};
     use stm32f1xx_hal::{
@@ -23,7 +23,7 @@ mod app {
         pac::{I2C2, TIM2, TIM4},
         prelude::*,
         pwm::{C1, C2, C3, C4, PwmChannel},
-        serial::{Config, Serial, Tx, Event, RxDma1},
+        serial::{Config, Serial, Tx, Event, RxDma2},
     };
 
     use systick_monotonic::*;
@@ -50,8 +50,8 @@ mod app {
 
     #[local]
     struct Local {
-        recv: Option<CircBuffer<[u8; COMMAND_SIZE], RxDma1>>,
-        usart1_tx: Tx<USART1>,
+        recv: Option<CircBuffer<[u8; COMMAND_SIZE], RxDma2>>,
+        usart2_tx: Tx<USART2>,
         pwm: PWM,
         pwm_tim: CountDownTimer<TIM2>,
         count: u32,
@@ -75,23 +75,23 @@ mod app {
 
         // BLUETOOTH
         let mut gpioa = dp.GPIOA.split();
-        let usart1_pins = (
-            gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh),
-            gpioa.pa10,
+        let usart2_pins = (
+            gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl),
+            gpioa.pa3,
         );
 
-        let mut usart1 = Serial::usart1(
-            dp.USART1,
-            usart1_pins,
+        let mut usart2 = Serial::usart2(
+            dp.USART2,
+            usart2_pins,
             &mut afio.mapr,
             Config::default().baudrate(9600.bps()),
             clocks,
         );
-        usart1.listen(Event::Idle);
+        usart2.listen(Event::Idle);
 
         let dma1 = dp.DMA1.split();
-        let (usart1_tx, rx) = usart1.split();
-        let rrx = rx.with_dma(dma1.5);
+        let (usart2_tx, rx) = usart2.split();
+        let rrx = rx.with_dma(dma1.6);
 
         let buf = cortex_m::singleton!(: [[u8; COMMAND_SIZE]; 2] = [[0; COMMAND_SIZE]; 2]).unwrap();
         let rx_transfer = rrx.circ_read(buf);
@@ -136,15 +136,15 @@ mod app {
         channels.2.enable();
         channels.3.enable();
 
-        //
-        // let mpu = Mpu6050::new(i2c2);
-        // mpu_init::spawn_after(1.secs(), mpu);
+
+        let mpu = Mpu6050::new(i2c2);
+        mpu_init::spawn_after(1.secs(), mpu);
 
         (
             Shared {},
             Local {
                 recv: Some(rx_transfer),
-                usart1_tx,
+                usart2_tx,
                 pwm: channels,
                 pwm_tim,
                 count: 0,
@@ -157,13 +157,13 @@ mod app {
     // #[task(binds = TIM2, local = [count, pwm, pwm_tim], priority = 2)]
     // fn motors(cx: motors::Context) {
     //     rprintln!("TIM TRIGGER");
-    //     // if *cx.local.count % 2 == 0 {
-    //     //     cx.local.pwm.0.set_duty(0);
-    //     //     cx.local.pwm.1.set_duty(0);
-    //     //     cx.local.pwm.2.set_duty(0);
-    //     //     cx.local.pwm.3.set_duty(0);
-    //     //     rprintln!("DUTY ZERO");
-    //     // } else {
+    //     if *cx.local.count % 2 == 0 {
+    //         cx.local.pwm.0.set_duty(0);
+    //         cx.local.pwm.1.set_duty(0);
+    //         cx.local.pwm.2.set_duty(0);
+    //         cx.local.pwm.3.set_duty(0);
+    //         rprintln!("DUTY ZERO");
+    //     } else {
     //         // let max_duty = (1.0 * cx.local.pwm.0.get_max_duty() as f32) as u16;
     //         let max_duty = u16::MAX;
     //         cx.local.pwm.0.set_duty(max_duty);
@@ -171,7 +171,7 @@ mod app {
     //         cx.local.pwm.2.set_duty(max_duty);
     //         cx.local.pwm.3.set_duty(max_duty);
     //         rprintln!("DUTY MAX");
-    //     // }
+    //     }
     //     *cx.local.count += 1;
 
     //     cx.local.pwm_tim.clear_update_interrupt_flag();
@@ -193,9 +193,9 @@ mod app {
         gyro::spawn(mpu, offset, spatial_orientation);
     }
 
-    #[task(local = [usart1_tx], capacity = 1)]
+    #[task(local = [usart2_tx], capacity = 1)]
     fn gyro(cx: gyro::Context, mut mpu: MPU, offset: Vector3<f32>, mut s: SpatialOrientation) {
-        let tx: &mut Tx<USART1> = cx.local.usart1_tx;
+        let tx: &mut Tx<USART2> = cx.local.usart2_tx;
         let spawn_next_at = monotonics::now() + 4.micros();
 
         let raw_gyro = mpu.get_gyro().expect("unable to get gyro");
@@ -203,7 +203,7 @@ mod app {
 
         s.adjust(raw_gyro - offset, angles);
 
-        // rprintln!("{:?}", s);
+        rprintln!("{:?}", s);
         // IntoIterator::into_iter(s.to_byte_array())
         //     .for_each(|byt| { nb::block!(tx.write(byt)).unwrap() });
         // nb::block!(tx.write(EOT)).unwrap();
@@ -212,31 +212,30 @@ mod app {
     }
 
     // todo: restore
-    // #[task(binds = USART1, local = [recv, pwm], priority = 2)]
-    // fn on_rx(cx: on_rx::Context) {
-    //     if let Some(rx) = cx.local.recv.take() {
-    //         let (buf, mut rx) = rx.stop();
-    //         let len = (buf[0].len() as u32 * 2) - rx.channel.ch().ndtr.read().bits();
+    #[task(binds = USART2, local = [recv, pwm], priority = 2)]
+    fn on_rx(cx: on_rx::Context) {
+        if let Some(rx) = cx.local.recv.take() {
+            let (buf, mut rx) = rx.stop();
+            let len = (buf[0].len() as u32 * 2) - rx.channel.ch().ndtr.read().bits();
 
-    //         let command = Command::from_byte_slice(&buf[0]);
-    //         rprintln!("got {:?}", command);
+            let command = Command::from_byte_slice(&buf[0]);
+            rprintln!("got {:?}", command);
 
-    //         if command.throttle <= 1.0 && command.throttle >= 0.0 {
-    //             let max_duty = cx.local.pwm.0.get_max_duty();
-    //             // let max_duty: u16 = u16::MAX;
-    //             let duty = (max_duty as f32 * command.throttle) as u16;
-    //             cx.local.pwm.0.set_duty(duty);
-    //             cx.local.pwm.1.set_duty(duty);
-    //             cx.local.pwm.2.set_duty(duty);
-    //             cx.local.pwm.3.set_duty(duty);
-    //             rprintln!("duty {}", duty);
-    //         }
+            if command.throttle <= 1.0 && command.throttle >= 0.0 {
+                let max_duty: u16 = u16::MAX;
+                let duty = (max_duty as f32 * command.throttle) as u16;
+                cx.local.pwm.0.set_duty(duty);
+                cx.local.pwm.1.set_duty(duty);
+                cx.local.pwm.2.set_duty(duty);
+                cx.local.pwm.3.set_duty(duty);
+                rprintln!("duty {}", duty);
+            }
 
-    //         let (rx, channel) = rx.release();
-    //         rx.clear_idle_interrupt();
-    //         let rx = rx.with_dma(channel);
+            let (rx, channel) = rx.release();
+            rx.clear_idle_interrupt();
+            let rx = rx.with_dma(channel);
 
-    //         cx.local.recv.replace(rx.circ_read(buf));
-    //     }
-    // }
+            cx.local.recv.replace(rx.circ_read(buf));
+        }
+    }
 }
